17aa04518067ef778304418a7c221bfcb03efcbe
Harrison <35530362+Nano112@users.noreply.github.com>
2023-03-17 20:31:11 +0000

    Cleaned up main file Moved translations in own folder

---
+++ src/simpleASM_compiler_main.py
@@ -0,0 +1,37 @@
+def core(translations):
+    print(f"core translations: {translations}")
+
+
+def basic(translations):
+    print(f"basic translations: {translations}")
+
+
+def advanced(translations):
+    print(f"advanced translations: {translations}")
+
+
+types = {
+    "core": {"filename": "core_translations.txt", "function": core},
+    "basic": {"filename": "basic_translations.txt", "function": basic},
+    "advanced": {"filename": "advanced_translations.txt", "function": advanced},
+}
+
+
+def get_translation_level():
+    while True:
+        type = input(
+            "what translation level should the compiler use? 'core' 'basic' 'advanced': "
+        )
+        if type.lower() in types:
+            break
+        print("invalid level ")
+    return type.lower()
+
+
+def startup(type):
+    with open(f"src\\translations\\{types[type]['filename']}", "r") as file:
+        translations = file.readlines()
+        types[type]["function"](translations)
+
+
+startup(get_translation_level())
